06f296ca46d25ed6c14a6884af2b098ac0f1bea2
Sameer Naik <sameer@fuloi.com>
2023-02-15 14:09:58 +0530

    adds hack/tools

---
+++ hack/tools.go
@@ -0,0 +1,27 @@
+//go:build tools
+// +build tools
+
+/*
+Copyright 2022 TriggerMesh Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package hack
+
+// These imports ensure build tools are included in Go modules so that we can
+// `go install` them in module-aware mode.
+// See https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+import (
+	_ "github.com/onsi/ginkgo/v2/ginkgo"
+)
